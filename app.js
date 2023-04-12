f3d116812f11d628a0d3a62dbbe99a452a8c7f4f
Mert Okumusoglu <mertokumusoglu4@gmail.com>
2023-04-12 18:17:32 +0300

    Server is working, mw tried

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const port = 3000
 const app = express()
+app.use(express.static('public'));
+const path = require("path")
+// Middlewares
 
 app.get("/", (req, res) => {
-    res.send("merhaba")
+    res.sendFile(path.resolve(__dirname, "temp/index.html"))
 })
 
 app.listen(3000, () => {
-    console.log("server başlatıldı cheeeeeeeck")
+    console.log("sunucu baslatildi 1")
 })
