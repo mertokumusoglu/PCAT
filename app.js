99d4eb0a4247b4d4cc13659a8206f13d3922570b
Mert Okumusoglu <mertokumusoglu4@gmail.com>
2023-04-13 01:15:31 +0300

    Last changes

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require("express")
+const ejs = require("ejs")
+
 const port = 3000
 const app = express()
+
+app.set("view engine", "ejs")
 app.use(express.static('public'));
+
 const path = require("path")
+
 // Middlewares
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "temp/index.html"))
+    res.render('index');
+  });
+
+app.get("/about", (req, res) => {
+    res.render('about')
+})
+
+app.get("/add", (req, res) => {
+    res.render('add')
 })
 
 app.listen(3000, () => {
